4d7fede008d600d1db9e0886617aa1449cb97bc0
Dave Head-Rapson <dave@dhrapson.com>
2017-01-12 12:34:58 +0000

    configure lambda function with s3/ email / dropbox parameters

--- infra.tf
+++ infra.tf
@@ -18,6 +18,14 @@ resource "aws_iam_role_policy" "s3notifier_lambda_iam_role_policy" {
         "logs:*"
       ],
       "Resource": "arn:aws:logs:*:*:*"
+    },
+     {
+      "Effect": "Allow",
+      "Action": [
+        "s3:Get*",
+        "s3:List*"
+      ],
+      "Resource": "arn:aws:s3:::*"
     }
   ]
 }
@@ -50,4 +58,17 @@ resource "aws_lambda_function" "s3notifier" {
     runtime = "java8"
     handler = "com.wtr.s3notifier.S3EventHandler"
     source_code_hash = "${base64sha256(file("target/s3notifier-function.jar"))}"
+    environment {
+        variables = {
+            SMTP_HOST = "${var.smtp_host}"
+            SMTP_PORT = "${var.smtp_port}"
+            SMTP_USERNAME = "${var.smtp_username}"
+            SMTP_PASSWORD = "${var.smtp_password}"
+            EMAIL_FROM = "${var.email_from}"
+            EMAIL_TO = "${var.email_to}"
+            DROPBOX_ACCESS_TOKEN  ="${var.dropbox_access_token}"
+        }
+    }
+    timeout = 20
+    memory_size = 256
 }
