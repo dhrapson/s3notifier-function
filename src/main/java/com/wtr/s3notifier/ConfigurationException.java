a9f12dd9cc596ee227502f2a650d0a2fc6b03356
Dave Head-Rapson <dave@dhrapson.com>
2017-01-12 12:27:22 +0000

    add environment variable loading class, aid to testing

---
+++ src/main/java/com/wtr/s3notifier/ConfigurationException.java
@@ -0,0 +1,11 @@
+package com.wtr.s3notifier;
+
+public class ConfigurationException  extends RuntimeException {
+
+	public ConfigurationException(String message) {
+		super(message);
+	}
+
+	private static final long serialVersionUID = 2362837462478L;
+	
+}
