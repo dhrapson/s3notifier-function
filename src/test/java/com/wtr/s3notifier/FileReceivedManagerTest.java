880b859b3bcd7c7b748c7720f2c8ad097c6003e9
Dave Head-Rapson <dave@dhrapson.com>
2017-01-19 09:14:52 +0000

    implement missing unit test for FileReceivedManager processAll

--- src/test/java/com/wtr/s3notifier/FileReceivedManagerTest.java
+++ src/test/java/com/wtr/s3notifier/FileReceivedManagerTest.java
@@ -1,11 +1,14 @@
 package com.wtr.s3notifier;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.eq;
 
 import java.io.File;
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Date;
 
 import org.junit.Test;
@@ -13,7 +16,9 @@ import org.mockito.Mockito;
 
 import com.wtr.s3notifier.dropbox.DropboxManager;
 import com.wtr.s3notifier.email.EmailManager;
+import com.wtr.s3notifier.s3.S3File;
 import com.wtr.s3notifier.s3.S3FileManager;
+import com.wtr.s3notifier.s3.S3FileSet;
 
 public class FileReceivedManagerTest {
 	
@@ -47,5 +52,22 @@ public class FileReceivedManagerTest {
 		when(s3.downloadFile("integrator", "client/INPUT/file")).thenReturn(f);
 		
 		assertEquals(false, managerSpy.process(cdf));
+	}
+	
+	@Test
+	public void testProcessAll() throws Exception {
+		
+		S3FileManager s3 = Mockito.mock(S3FileManager.class);
+		FileReceivedManager managerReal = new FileReceivedManager(s3, null, null, "test@example.com");
+		FileReceivedManager managerSpy = Mockito.spy(managerReal);
+		Date lastMod = new Date();
+		when(s3.listBucketNames()).thenReturn(Arrays.asList("one", "two"));
+		when(s3.listFiles("one")).thenReturn(new S3FileSet(Arrays.asList(new S3File("one", "client/PROCESSED/keyA", new Date()))));
+		when(s3.listFiles("two")).thenReturn(new S3FileSet(Arrays.asList(new S3File("two", "client/INPUT/keyA", new Date()), new S3File("two", "client/PROCESSED/keyB", lastMod), new S3File("two", "client/PROCESSED/keyC", lastMod))));
+		
+		ClientDataFile twoA = new ClientDataFile("/foo", "two", "client/INPUT/keyA", lastMod);
+		doReturn(true).when(managerSpy).process(twoA);
+		assertEquals(1, managerSpy.processAll("/foo").size());		
+		verify(managerSpy).process(eq(twoA));
 	}
 }
