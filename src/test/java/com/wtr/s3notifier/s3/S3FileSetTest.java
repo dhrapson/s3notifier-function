f4b9542a05d0c74c5d5f7d5a163b66caedc3cefa
Dave Head-Rapson <dave@dhrapson.com>
2017-01-19 09:09:23 +0000

    model S3FileSet as custom type

---
+++ src/test/java/com/wtr/s3notifier/s3/S3FileSetTest.java
@@ -0,0 +1,19 @@
+package com.wtr.s3notifier.s3;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Date;
+
+import org.junit.Test;
+
+public class S3FileSetTest {
+	
+	@Test
+	public void testSubsetMatching() {
+		S3FileSet set = new S3FileSet(Arrays.asList(new S3File("bucket1", "key1", new Date()), new S3File("bucket1", "key2", new Date())));
+		assertEquals(set.subsetMatching("bucket1", "key(.*)").size(), 2);
+		assertEquals(set.subsetMatching("bucket1", "key1(.*)").size(), 1);
+		assertEquals(set.subsetMatching("bucket1", "foo(.*)").size(), 0);
+	}
+}
